9fcedf2166dfe8fab77a74b0b67faf92df28dbae
gopf <44543690+gopf@users.noreply.github.com>
2020-10-25 18:55:09 +0000

    add setup script

---
+++ setup-git.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+git config --global alias.st "status -s"
+git config --global alias.ci commit
+git config user.name "gopf"
+git config user.email "44543690+gopf@users.noreply.github.com"
+git config credential.helper store
